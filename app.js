6d237157d78817462bc3c0f32255e9816f330ceb
Dante Piekart <dantepiekart@gmail.com>
2024-06-18 19:59:33 +0200

    Error Handling
    - Added 404
    - Added 500

--- app.js
+++ app.js
@@ -11,6 +11,15 @@ app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "src/views"));
 
+app.use((req, res, next) => {
+  res.status(404).render("pages/404.ejs");
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).render("pages/500.ejs");
+});
+
 app.listen(process.env.PORT, () => {
   console.log("Node running on port", process.env.PORT);
 });
