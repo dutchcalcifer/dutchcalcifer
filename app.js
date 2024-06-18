634860180892b0f168b801a36e8993d8cc2ae746
Dante Piekart <dantepiekart@gmail.com>
2024-06-18 13:37:14 +0200

    Boilerplate
    - Setup routes
    - Setup EJS
    - Setup .env
    - Setup file structure

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-import express from "express";
-
+const express = require("express");
+const path = require("path");
+const dotenv = require("dotenv").config();
 const app = express();
-const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello world!");
-});
+const routes = require("./src/routes/routes.js");
+app.use("/", routes);
 
-app.listen(PORT, () => {
-  console.log(`Express server running at http://localhost:${PORT}/`);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "src/views"));
+
+app.listen(process.env.PORT, () => {
+  console.log("Node running on port", process.env.PORT);
 });
