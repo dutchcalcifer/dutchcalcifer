698c14efce3b7bd2bc3e577dd8239564280413fe
Dante Piekart <dantepiekart@gmail.com>
2024-06-20 16:17:04 +0200

    4.0.0
    - complete rework

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,11 +1,29 @@
 const express = require("express");
 const router = express.Router();
+const fs = require("fs");
 
 router.get("/", async (req, res) => {
   try {
-    res.render("index");
+    fs.readFile("src/data/db.json", "utf8", (err, data) => {
+      if (err) {
+        console.error(err);
+        res.status(500).render("pages/500.ejs");
+        return;
+      }
+      const randomMessage =
+        JSON.parse(data).motd[
+          Math.floor(Math.random() * JSON.parse(data).motd.length)
+        ];
+
+      const randomPalete =
+        JSON.parse(data).palate[
+          Math.floor(Math.random() * JSON.parse(data).palate.length)
+        ];
+      res.render("index", { randomMessage, randomPalete });
+    });
   } catch (error) {
     console.error(error);
+    res.status(500).render("pages/500.ejs");
   }
 });
 
